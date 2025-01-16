48380e3b5c0da2fd18b25ec363f185bd75015abc
Soner Sayakci <s.sayakci@gmail.com>
2025-01-16 19:39:32 +0100

    fix: installer

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,10 +1,14 @@
-import { setFailed } from "@actions/core";
+import { addPath, setFailed } from "@actions/core";
 import { inputs } from './context';
 import { install } from "./installer";
+import { dirname } from 'path';
 
 async function run() {
     try {
         const { bin, version } = await install(inputs.version);
+
+        const releaser = dirname(bin);
+        addPath(releaser);
     } catch (error) {
         if (error instanceof Error) {
             setFailed(error.message);
